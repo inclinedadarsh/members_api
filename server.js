ec6f86618a1edf75ef80e19968a12da6f875983b
inclinedadarsh <dubeyadarshmain@gmail.com>
2023-03-21 14:48:41 +0530

    Added setup code In server.js: set up express set up mongoose and connected with mongodb set up basic middleware to accept JSON started the server started setting up routes. Will do it next.

---
+++ server.js
@@ -0,0 +1,25 @@
+// Importing the dotenv file
+require("dotenv").config();
+
+// Setting up express
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Setting up Mongoose and connection with the MongoDB
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
+
+// Setting up Epxress middleware to accept JSON requests
+app.use(express.json());
+
+// Setting up Routes
+const membersRoutes = require("./routes/members");
+
+// Starting the server
+app.listen(PORT, () => {
+  console.log("Server started!");
+});
